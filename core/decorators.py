92ee0553a5b970854de86720337197f73513954c
Niyaz Yakupov <widua33@gmail.com>
2017-11-14 10:09:10 +0300

    a skeleton forRBAC decorators

--- core/decorators.py
+++ core/decorators.py
@@ -1,12 +1,38 @@
 # -*- coding: utf-8 -*-
+#from django.views.decorators.http import
+#from django.views.decorators.csrf import
+#from django.contrib.auth.decorators import
+
+
+def customer_required():
+    """
+    Decorator that checks if a user is currently logged in, has a profile and a 'customer' role
+    """
+    def check_role(request):
+        return True
 
 """
-def customer_required():
-    pass
-
 def producer_required():
     pass
 
 def profile_required():
     pass
+"""
+
+"""
+def permission_required(perm, login_url=None, raise_exception=False):
+    def check_perms(user):
+        if isinstance(perm, six.string_types):
+            perms = (perm, )
+        else:
+            perms = perm
+        # First check if the user has the permission (even anon users)
+        if user.has_perms(perms):
+            return True
+        # In case the 403 handler should be called raise the exception
+        if raise_exception:
+            raise PermissionDenied
+        # As the last resort, show the login form
+        return False
+    return user_passes_test(check_perms, login_url=login_url)
 """
