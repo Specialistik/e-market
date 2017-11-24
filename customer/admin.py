b152a77ef5a294106cee7aa8b2e0d09754023da1
Niyaz Yakupov <widua33@gmail.com>
2017-11-24 18:29:02 +0300

    admin thingies

--- customer/admin.py
+++ customer/admin.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import OrderStatuses
+from .models import OrderStatuses, Order, OrderUnit
 
 admin.site.register(OrderStatuses)
+admin.site.register(Order)
+admin.site.register(OrderUnit)
