2089c4bdeaed23deefbc50a5753c4dc71baa00d1
Niyaz Yakupov <widua33@gmail.com>
2018-01-26 13:06:48 +0300

    danone bug

--- static/js/products.js
+++ static/js/products.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     $('.add_to_cart').click(function(){
-        console.log('add to cart was clicked');
         var product = $(this).closest('.col_products');
         var minimal_price = product.find('.product_minimum_amount').html();
 
@@ -25,14 +24,12 @@ $(document).ready(function(){
     });
 
     $('.modal_desc_product').click(function(){
-        console.log('product pic was clicked');
         var product = $(this).closest('.col_products');
         $('#popup_minimum_amount_desc').html('от ' + product.find('.product_minimum_amount').html() + ' шт.');
         $('#popup_product_url_desc').attr('src', product.find('.img_url').attr('src'));
         $('#popup_price_desc').html(product.find('.products_price').html());
         $('#popup_description_desc').html(product.find('.product_description').html());
-        //$(this).magnificPopup({src: '#modal_desc_product'});
-        //$('#modal_desc_product').magnificPopup({type: 'inline', src: '#modal_desc_product'});
+        $('#popup_name_desc').html(product.find('.product_name').html());
          $.magnificPopup.open({
             items: {
                 src: '#modal_desc_product'
