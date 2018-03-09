c4de18ddf357cbd027e77c081e390f8ae28c2cc5
DESKTOP-23PF8VD\Дмитрий <zdimaz.dudko@gmail.com>
2018-03-09 20:27:19 +0200

    4. При выпадании списка адресов в заказе добавить сверху input, при введении текста в который фильтровать список

--- static/js/basket.js
+++ static/js/basket.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
     $('#popup_tradepoint').change(function(){
         $('#proxy_tradepoint').val($(this).val());
     });
@@ -14,6 +15,7 @@ $(document).ready(function(){
         e.preventDefault();
         $('#perform_order_form').submit();
     });
+
     /*
     $('#tradepoint_pick').magnificPopup({
         items: {
@@ -22,4 +24,58 @@ $(document).ready(function(){
         }
     });
     */
+
+    /* ------------------------------------------------
+        multisearch
+    ------------------------------------------------ */
+
+        if($("#myMultiSearch").length){
+
+            var deliveryAddress = [
+               { name: 'ул. А. Варакина, 21, Нефтеюганск, Ханты-Мансийский автономный округ, Россия'},
+               { name: 'ул. А. Варакина, 114, Россия'}
+            ];
+
+            $("#myMultiSearch").multisearch({
+                inputPosition: 'start',
+                maxShowOptions: 5,
+                minSearchChars: 1,
+                source: deliveryAddress,
+
+                keyAttrs: ["name"],
+
+                formatPickerItem: function( data ) {
+                    return '<li><a href="javascript:;">'+data.name+'</a></li>';
+                },
+                formatSelectedItem: function( data ) {
+                    return '<a href="javascript:;" class="selected_item"><span>'+data.name+
+                    '</span><i class="fa fa-times-circle multisearch_clear"></i></a>';
+                },
+
+            });
+
+            // close item
+            $(document).on('click', '.multisearch_clear', function(){
+
+                var text = $(this).parent().find('span').text(),
+                    id;
+
+                var el = deliveryAddress.find(function arrFind(element, index, array){
+                    console.log(text)
+                    if (element.name == text) {
+                      return element;
+                    }
+                });
+
+                $('#myMultiSearch').multisearch('remove', el);
+
+            });
+            // close item
+
+        }
+
+    /* ------------------------------------------------
+        End of multisearch
+    ------------------------------------------------ */
+
 });
