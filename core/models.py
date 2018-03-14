0fdcd7fc8b5445e050cf443e5f51482a1b5c71af
Niyaz Yakupov <widua33@gmail.com>
2018-03-14 15:37:08 +0300

    Индекс необязательный

--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.gis.db.models import PointField, PolygonField
+from django.contrib.gis.db.models import PointField
 from catalogs.models import AbstractList
 
 from customer.models import TradePoint
