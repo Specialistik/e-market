fe6a77b6db845891ed7d85eac93e6752c04e5e32
Niyaz Yakupov <widua33@gmail.com>
2018-03-27 10:42:58 +0300

    bugfixes

--- payments/admin.py
+++ payments/admin.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
 
 from .models import OrderPayment, PaymentNotification, Success, Failure
 
 
+@admin.register(OrderPayment)
 class OrderPaymentAdmin(admin.ModelAdmin):
     list_display = ('customer', 'price', 'created', 'type')
 
 
+@admin.register(Success, Failure, PaymentNotification)
 class SuccessFailureNotificationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'customer')
-
-
-admin.site.register(OrderPayment, OrderPaymentAdmin)
-admin.site.register(Success, Failure, PaymentNotification)(SuccessFailureNotificationAdmin)
+    list_display = ('response', 'customer')
