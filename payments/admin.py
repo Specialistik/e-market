a6e82f6be1db033e39b5d7551bce3d942e81c69d
Niyaz Yakupov <widua33@gmail.com>
2018-03-27 10:38:55 +0300

    final payments admin isues

--- payments/admin.py
+++ payments/admin.py
@@ -3,9 +3,16 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import PaymentNotification, Success, Failure
+from .models import OrderPayment, PaymentNotification, Success, Failure
 
 
-admin.site.register(PaymentNotification)
-admin.site.register(Success)
-admin.site.register(Failure)
+class OrderPaymentAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'price', 'created', 'type')
+
+
+class SuccessFailureNotificationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'customer')
+
+
+admin.site.register(OrderPayment, OrderPaymentAdmin)
+admin.site.register(Success, Failure, PaymentNotification)(SuccessFailureNotificationAdmin)
