ca1336df967e417e7f86c4feef5a18f0c76afb7a
Specialistik <test04@list.ru>
2018-04-06 11:19:18 +0300

    sorting of customers

--- manager/views.py
+++ manager/views.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+
+from collections import OrderedDict
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
@@ -21,15 +23,15 @@ def my_personal_data(request):
 
 @login_required(login_url='/sign_in/')
 def my_clients(request):
-    """
+
     def sort_by_income(tp_set):
         def compose_sum(trade_point):
-            return trade_point.composite_sum()
+            return trade_point.customer.customer_total()
 
         sorted_tps = list(tp_set)
         sorted_tps.sort(key=compose_sum)
         return sorted_tps
-    """
+
     if hasattr(request.user, 'profile'):
         if request.user.profile.role == 'manager':
             customers = {}
@@ -37,36 +39,31 @@ def my_clients(request):
             if 'search_string' in request.GET:
                 trade_point_queryset = trade_point_queryset.filter(address__full_address__icontains=request.GET['search_string'])
 
-            if 'sort' in request.GET:
-                sort_type = request.GET['sort']
-                #if sort == 'income':
-                #trade_point_queryset = sort_by_income(trade_point_queryset)
-                """
-                def compose_sum(trade_point):
-                    return trade_point.composite_sum()
-
-                sorted_tps = list(trade_point_queryset)
-                trade_point_queryset = sorted_tps.sort(key=compose_sum)
-                """
-
-                #if request.GET['sort'] == 'name':
-                #    trade_point_queryset = trade_point_queryset.order_by('customer__profile__company_name')
-                #    print trade_point_queryset.query
-            else:
-                sort_type = 'income'
-                #trade_point_queryset = sort_by_income(trade_point_queryset)
+            sort_type = request.GET['sort'] if 'sort' in request.GET else 'income'
 
             for tp in trade_point_queryset:
-                if tp.customer_id in customers.keys():
-                    customers[tp.customer_id]['trade_points'].append(tp)
+
+                if sort_type == 'name':
+                    sorted_key = tp.customer.profile.company_name
                 else:
-                    customers[tp.customer_id] = {
+                    sorted_key = tp.customer.customer_total()
+
+                if sorted_key in customers.keys():
+                    customers[sorted_key]['trade_points'].append(tp)
+                else:
+                    customers[sorted_key] = {
                         "company_name": tp.customer.profile.company_name,
                         "signer_fio": (tp.customer.profile.signer_info.fio() if tp.customer.profile.signer_info else ''),
                         "phone": tp.customer.profile.nice_phone(),
                         "customer_total": tp.customer.customer_total(),
                         "trade_points": [tp, ]
                     }
+
+                customers = OrderedDict(sorted(customers.items()))
+            #if sort_type == 'name':
+                #customers = sorted(customers, key=lambda customer: customer['company_name'])
+                #customers.sort(key=lambda d: d.values()['company_name'], reversed=True)
+                #OrderedDict(sorted(customers.items(), key='company_name'))
             return render(request, 'manager/my_clients.html', {
                 'profile': request.user.profile,
                 'customers': customers,
