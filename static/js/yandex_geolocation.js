473931feaf313f9a139955b9af3d8bd2438fc137
Specialistik <test04@list.ru>
2018-04-18 17:04:09 +0300

    only exact address autocomplete

--- static/js/yandex_geolocation.js
+++ static/js/yandex_geolocation.js
@@ -5,7 +5,8 @@ $(document).ready(function(){
         function get_name_by_kind(object, name, related_kind) {
             $.each(object, function(key, value){
                 if (value.kind === name) {
-                    $('.autocomplete_' + related_kind).val(value.name);
+                    overlord.find('.autocomplete_' + related_kind).val(value.name);
+                    //$('.autocomplete_' + related_kind).val(value.name);
                 }
             });
         }
