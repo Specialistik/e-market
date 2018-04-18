58e8959ad2a4ad9645e2e997cab0346b752c6ec7
Specialistik <test04@list.ru>
2018-04-18 15:08:09 +0300

    api compiler fixes

---
+++ src/auth/api.jsx
@@ -0,0 +1,53 @@
+// file: src/util/Auth.js
+import axios from 'axios';
+import _ from 'lodash';
+import store from '../store';
+import { setToken } from '../actions'
+
+export function InvalidCredentialsException(message) {
+    this.message = message;
+    this.name = 'InvalidCredentialsException';
+}
+
+export function login(username, password) {
+  return axios
+    .post('/api/sign_in', {
+      username,
+      password
+    })
+    .then(function (response) {
+      store.dispatch(setToken(response.data.token));
+    })
+    .catch(function (error) {
+      // raise different exception if due to invalid credentials
+      if (_.get(error, 'response.status') === 400) {
+        throw new InvalidCredentialsException(error);
+      }
+      throw error;
+    });
+}
+
+export function loggedIn() {
+  return store.getState().token == null;
+}
+
+export function register(company_name, ) {
+    return axios
+    .post('/api/sign_up', {
+        company_name,
+        inn,
+        password,
+        email,
+        phone,
+      })
+      .then(function (response) {
+        store.dispatch(setToken(response.data.token));
+      })
+      .catch(function (error) {
+        // raise different exception if due to invalid credentials
+        if (_.get(error, 'response.status') === 400) {
+          throw new InvalidCredentialsException(error);
+        }
+        throw error;
+      });
+}
