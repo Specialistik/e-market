4da1eca3b1f2f7110431a5263fcae2b47326905e
Specialistik <test04@list.ru>
2018-07-24 14:43:31 +0300

    relatively stable

--- android/app/build/generated/source/buildConfig/debug/com/the_sklad/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/debug/com/the_sklad/BuildConfig.java
@@ -5,9 +5,9 @@ package com.the_sklad;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.the_sklad";
+  public static final String APPLICATION_ID = "ru.the_sklad.app";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "1.01";
+  public static final String VERSION_NAME = "1";
 }
