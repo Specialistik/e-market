261e828b8f3df6a81c594f6526016d2c8ed2b66f
specialistik <test04@list.ru>
2018-08-30 13:59:24 +0300

    depots admin

--- producer/admin.py
+++ producer/admin.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
-from models import ProductCard
+from models import ProductCard, ProducerDepot
 
 admin.site.register(ProductCard)
+admin.site.register(ProductDepot)
